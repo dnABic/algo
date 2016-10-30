60764e89abbbe1e2bf8cf17afc95f1fa45b0d579
dnABic <dnABic@localhost>
2016-10-30 23:09:44 +0100

    First working version

--- BubbleSort/main.go
+++ BubbleSort/main.go
@@ -2,8 +2,60 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
+func intInSlice(number int, list []int) bool {
+	for _, element := range list {
+		if number == element {
+			return true
+		}
+	}
+	return false
+}
+
+func randomInt(min, max int) int {
+	return rand.Intn(max-min) + min
+}
+
+func sweep(numbers []int, passes int) bool {
+	var tempNum int
+	var didSwap bool = false
+
+	for cnt := 0; cnt < (len(numbers) - 1 - passes); cnt++ {
+		if numbers[cnt] > numbers[cnt+1] {
+			tempNum = numbers[cnt+1]
+			numbers[cnt+1] = numbers[cnt]
+			numbers[cnt] = tempNum
+			didSwap = true
+		}
+	}
+
+	return didSwap
+}
+
+func bubbleSort(numbers []int) {
+	for passes := 0; passes < len(numbers); passes++ {
+		if !sweep(numbers, passes) {
+			break
+		}
+	}
+}
+
 func main() {
-	fmt.Println("vim-go")
+	var randNumbers []int
+
+	rand.Seed(time.Now().Unix())
+
+	for len(randNumbers) < 20 {
+		randInt := randomInt(1, 100)
+		if !intInSlice(randInt, randNumbers) {
+			randNumbers = append(randNumbers, randInt)
+		}
+	}
+	fmt.Println("Unsorted:", randNumbers)
+
+	bubbleSort(randNumbers)
+	fmt.Println("Sorted:", randNumbers)
 }
