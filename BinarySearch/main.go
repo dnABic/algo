4ea41b37c76be967cfdd686fbbc4b37785972cdb
dnABic (Andreja Babic) <ababic@gmail.com>
2016-12-20 01:00:27 +0100

    Working version

--- BinarySearch/main.go
+++ BinarySearch/main.go
@@ -3,24 +3,27 @@ package main
 import "fmt"
 
 func binarySearch(sortedList []int, lookingFor int) int {
-	var middleIndex int = len(sortedList) / 2
-	var middleElement = sortedList[middleIndex]
-	fmt.Println("Middle element is:", middleElement)
+	first := 0
+	last := len(sortedList) - 1
 
-	if middleElement == lookingFor {
-		return middleIndex
-	} else if middleElement > lookingFor {
-		// search for lower part
-	} else {
-		// upper part
+	for first <= last {
+		middleIndex := first + (last-first)/2
+		middleElement := sortedList[middleIndex]
+		fmt.Println("Middle element is:", middleElement)
+
+		if middleElement == lookingFor {
+			return middleIndex
+		} else if middleElement > lookingFor {
+			last = middleIndex - 1
+		} else {
+			first = middleIndex + 1
+		}
 	}
-
 	return -1
-
 }
 
 func main() {
-	var lookingFor = 6
+	var lookingFor = 2
 	var sortedList []int = []int{1, 3, 4, 6, 7, 8, 10, 11, 14}
 	fmt.Println("Looking for", lookingFor, "in the sorted list:", sortedList)
 
